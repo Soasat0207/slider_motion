dcecd6d4bbfe6c0d51179d006c32dd23ee49ed8b
Soasat <lehuyhiep449@gmail.com>
2022-03-07 14:16:50 +0700

    first commit

---
+++ src/image.js
@@ -0,0 +1,10 @@
+import image1 from "./assets/image/1.jpeg";
+import image2 from "./assets/image/2.jpg";
+import image3 from "./assets/image/3.jpg";
+import image4 from "./assets/image/4.jpg";
+import image5 from "./assets/image/5.jpg";
+import image6 from "./assets/image/6.jpg";
+import image7 from "./assets/image/7.jpg";
+import image8 from "./assets/image/8.jpg";
+
+export default [image1, image2, image3, image4, image5, image6, image7, image8];
